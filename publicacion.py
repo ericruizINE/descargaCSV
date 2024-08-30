1c0535b870025dcf5299bf7cb3a34a0fb2085b6e
ericruizINE <eric.ruiz@ine.mx>
2024-08-29 18:59:42 -0600

    update test case

--- publicacion.py
+++ publicacion.py
@@ -160,16 +160,10 @@ try:
     @allure.feature('Validación de datos en sitio de Publicación')  # Usa etiquetas estándar de Allure
     @allure.story('1.- Validación de número de actas esperadas en Estadística Nacional')  # Usa etiquetas estándar de Allure
     @allure.tag('prioridad:alta', 'tipo:funcional')
-    def test_actas_esperadas_estadistica_nacional_coinciden(valor_con_comas2, screenshots_folder):
+    def test_actas_esperadas_estadistica_acional_coinciden():
         """
         Prueba que los valores de actas esperadas en Estadística Nacional coincidan con los valores del CSV.
         """
-        elemento3 = driver.find_element(By.XPATH, "/html/body/app-root/app-federal/div/div/div[3]/app-nacional/div/app-estadistica/div[1]/div[1]/div[2]/div[1]/p[1]/strong")
-        valor_en_pagina3 = elemento3.text
-
-        file_path = get_next_screenshot_path(screenshots_folder, 'actas_esperadas')
-        capture_element_screenshot(driver, elemento3, file_path)
-        
         with allure.step("Comparando los valores de sitio vs csv"):
             if valor_en_pagina3 == valor_con_comas2:
                 allure.attach(
@@ -177,28 +171,12 @@ try:
                     name="Resultado de la validación",
                     attachment_type=allure.attachment_type.TEXT
                 )
-                # Adjuntar la captura de pantalla en caso de error
-                with open(file_path, "rb") as image_file:
-                    allure.attach(
-                        image_file.read(),
-                        name="Captura del elemento",
-                        attachment_type=allure.attachment_type.PNG
-                    )
             else:
                 allure.attach(
                     f"1.- Los valores no coinciden, Sitio: {valor_en_pagina3} CSV: {valor_con_comas2}",
                     name="Resultado de la validación",
                     attachment_type=allure.attachment_type.TEXT
                 )
-                # Adjuntar la captura de pantalla en caso de error
-                with open(file_path, "rb") as image_file:
-                    allure.attach(
-                        image_file.read(),
-                        name="Captura de pantalla del error",
-                        attachment_type=allure.attachment_type.PNG
-                    )
-
-            # Verificación final
             assert valor_en_pagina3 == valor_con_comas2, (
                 "Los valores no coinciden. Revisa el reporte para más detalles."
             )
